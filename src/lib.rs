9caa710f698486a6996649a60999956638b90d6a
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-28 15:51:12 -0700

    start to reify type signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate serde_json;
+
 /*
    TODO:
    1) define pub sub actor Message Exchange protocols
@@ -11,3 +13,24 @@
    2) define setInterval as a message exchange
    3) derive example to demonstrate clock skew and correction
 */
+
+struct MessageExchange;
+impl MessageExchange {
+   pub fn publish(descriptor_prefix: &str) -> Outbox {
+      Outbox {}
+   }
+   pub fn subscribe(selector_prefix: &str, inbox: &Inbox) {
+   }
+}
+
+struct Outbox;
+impl Outbox {
+   pub fn push(descriptor_suffix: &str, msg: &serde_json::Value) {
+   }
+}
+
+struct Inbox;
+/* pub impl Inbox {
+   pub fn new(selector_suffix: &str, callback: ?) -> Inbox {
+   }
+} */
