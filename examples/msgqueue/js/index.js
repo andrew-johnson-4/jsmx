b5a2dd3878098319c0c364607323cb34513fc255
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-28 19:27:31 -0700

    define jsmx API to be exposed to JavaScript

--- examples/msgqueue/js/index.js
+++ examples/msgqueue/js/index.js
@@ -1,1 +1,9 @@
 import("../pkg/index.js").catch(console.error);
+
+jsmx_subscribe("setInterval","1000",function(msg) {
+   console.log("read message from javascript",msg);
+});
+
+setInterval(function(){
+   jsmx_push("setInterval","1000","message from javascript");
+}, 1000);
