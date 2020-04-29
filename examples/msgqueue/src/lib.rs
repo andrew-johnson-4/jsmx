12e088d80cfc0ce31fbf7a6552edffcac4db0a12
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-04-28 18:11:46 -0700

    same-thread messaging is working

--- examples/msgqueue/src/lib.rs
+++ examples/msgqueue/src/lib.rs
@@ -2,14 +2,14 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 extern crate jsmx;
-use jsmx::{MessageExchange, Outbox, Inbox};
+use jsmx::{MessageExchange, Inbox};
 use serde_json::{Value};
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    let exchange = MessageExchange::new();
+    let mut exchange = MessageExchange::new();
 
-    let inbox = Inbox::new("1000", move |msg| {
+    let inbox = Inbox::new("1000", move |_msg| {
         console::log_1(&JsValue::from_str("Hello mail inbox!"));
     });
     exchange.subscribe("setInterval", &inbox);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate serde_json;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 pub struct MessageExchange {
    boxes: HashMap<String, Vec<Inbox>>
@@ -10,25 +11,40 @@ impl MessageExchange {
          boxes: HashMap::new()
       }
    }
-   pub fn publish(&self, descriptor_prefix: &str) -> Outbox {
-      Outbox {}
+   pub fn publish(self, descriptor_prefix: &str) -> Outbox {
+      Outbox {
+         message_exchange: Box::new(self),
+         descriptor_prefix: descriptor_prefix.to_string()
+      }
    }
    pub fn subscribe(&mut self, selector_prefix: &str, inbox: &Inbox) {
       if !self.boxes.contains_key(selector_prefix) {
          self.boxes.insert(selector_prefix.to_string(), Vec::new());
       }
+      self.boxes.get_mut(selector_prefix).unwrap().push((*inbox).clone());
    }
 }
 
-pub struct Outbox;
+pub struct Outbox {
+   message_exchange: Box<MessageExchange>,
+   descriptor_prefix: String
+}
 impl Outbox {
    pub fn push(&self, descriptor_suffix: &str, msg: &serde_json::Value) {
+       if let Some(inboxes) = self.message_exchange.boxes.get(&self.descriptor_prefix) {
+          for inbox in inboxes.iter() {
+             if descriptor_suffix == inbox.selector_suffix {
+                (inbox.callback)(msg);
+             }
+          }
+       }
    }
 }
 
+#[derive(Clone)]
 pub struct Inbox {
    selector_suffix: String,
-   callback: Box<dyn Fn(&serde_json::Value)>
+   callback: Rc<Box<dyn Fn(&serde_json::Value)>>
 }
 impl Inbox {
    pub fn new<F>(selector_suffix: &str, callback: F) -> Inbox
@@ -36,7 +52,7 @@ impl Inbox {
    {
       Inbox {
          selector_suffix: selector_suffix.to_string(),
-         callback: Box::new(callback)
+         callback: Rc::new(Box::new(callback))
       }
    }
 }
